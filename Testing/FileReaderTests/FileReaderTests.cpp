56acd604598f32edffeb6a99481ed72dfa606a59
Charles.Steven.Thomson <Charles.Steven.Thomson@gmail.com>
2025-03-12 11:48:01 +1000

    Cleaned up dead code

--- Testing/FileReaderTests/FileReaderTests.cpp
+++ Testing/FileReaderTests/FileReaderTests.cpp
@@ -40,30 +40,11 @@ bool fileExists(const string& path) {
     return file.good();
 }
 
-
 //*
 // @ brief Test reading of test files
 // 
 //  Test if the test files can be read and return the expected data
 // */
-TEST(FileReaderTests, ReadTestFiles) {
-
-    for (const string& fileName : TEST_FILE_NAMES) {
-        string fullPath = TEST_INPUT_FILE_PATH + fileName + ".txt";
-
-        ASSERT_TRUE(fileExists(fullPath)) << "File does not exist" << fullPath;
-
-        FileReader newFileReader;
-
-        string testData = newFileReader.GetFileContents(fullPath);
-
-        ASSERT_TRUE(typeid(testData) == typeid(string)) << "Expected String got : " << typeid(testData).name();
-
-        ASSERT_FALSE(testData.empty()) << "File is empty : " << fullPath;
-    }
-
-}
-
 TEST(FileReaderTests, TestReadFile) {
     string fileName = "ValidMultiLine";
     string fullPath = TEST_INPUT_FILE_PATH + fileName + ".txt";
@@ -74,16 +55,17 @@ TEST(FileReaderTests, TestReadFile) {
 
     tuple<string, string> testKeysAndValues = GetTestFileKeysAndValues(fileName);
 
+    ASSERT_TRUE(typeid(result) == typeid(string)) << "Expected String got : " << typeid(result).name();
+
+    ASSERT_FALSE(result.empty()) << "File is empty : " << fullPath;
+
+
+
     cout << "Test Keys : " << get<0>(testKeysAndValues) << endl;
 
     cout << "Test Values : " << get<1>(testKeysAndValues) << endl;
 
-
-
     cout << "Test Return : " << result << endl;
-
-    /*ASSERT_EQ(1, 2);*/
-
 
 }
 
