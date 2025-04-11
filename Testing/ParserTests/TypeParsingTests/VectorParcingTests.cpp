e274f9a257c372bb2cab9a68c521ed3a6b2295e0
Charles.Steven.Thomson <Charles.Steven.Thomson@gmail.com>
2025-04-11 11:08:18 +1000

    Implmenting type testing for parser

---
+++ Testing/ParserTests/TypeParsingTests/VectorParcingTests.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+#include "CPP-JSONParser.h"
+
+#include <string>
+
+using std::string;
+
+
+TEST(TypeParcingTests, TypeParcing_Vecotor) {
+
+	cout << "In the vector parcing testsing" << endl;
+
+	ASSERT_EQ(1, 2);
+}
