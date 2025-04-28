fd13758f8236d6bd7b19dcfcef22b611722e6c69
Charles.Steven.Thomson <Charles.Steven.Thomson@gmail.com>
2025-04-28 11:14:04 +1000

    Cleaned up VectorParsingTests test failure message

--- Testing/ParserTests/TypeParsingTests/VectorParcingTests.cpp
+++ Testing/ParserTests/TypeParsingTests/VectorParcingTests.cpp
@@ -15,19 +15,15 @@ TEST(TypeParcingTests, TypeParcing_Vector_double) {
 
 	JSON testJson = ReadAndParseTypeTestFile(filePath);
 
-	cout << "TypeParcing_Vecotor  - > File Read OKAY" << endl;
-
 	JSON result = GetValueByKey(testJson, "test_vector");
 
 	string heldType = result->getType();
 
 	vector<double> testVector = GetValueByKeyWithType<vector<double>>(testJson, "test_vector");
 
-	SCOPED_TRACE(format("The expected return type vector<double> - The actual return type : {}", typeid(testVector).name()));
-	ASSERT_TRUE(typeid(testVector) == typeid(vector<double>));
-
-	SCOPED_TRACE(format("The size of the test vector : 3 - The size of the returned Vactor : {}", testVector.size()));
-	ASSERT_TRUE(testVector.size() == 3);
+	
+	ASSERT_TRUE(typeid(testVector) == typeid(vector<double>)) << "The expected return type vector<double> - The actual return type : " << typeid(testVector).name();
+	ASSERT_TRUE(testVector.size() == 3) << "The size of the test vector : 3 - The size of the returned Vactor : " << testVector.size();
 }
 
 
@@ -38,24 +34,15 @@ TEST(TypeParcingTests, TypeParcing_Vector_string) {
 
 	JSON testJson = ReadAndParseTypeTestFile(filePath);
 
-	cout << "TypeParcing_Vecotor  - > File Read OKAY" << endl;
-
 	JSON result = GetValueByKey(testJson, "test_vector");
 
 	string heldType = result->getType();
 
 	vector<string> testVector = GetValueByKeyWithType<vector<string>>(testJson, "test_vector");
 
-	SCOPED_TRACE(format("The expected return type vector<string> - The actual return type : {}", typeid(testVector).name()));
-	ASSERT_TRUE(typeid(testVector) == typeid(vector<string>));
-
-	SCOPED_TRACE(format("The size of the test vector : 3 - The size of the returned Vactor : {}", testVector.size()));
-	ASSERT_TRUE(testVector.size() == 3);
-
-	for (string val : testVector) {
-		cout << format("Vector Element : {}", val) << endl;
-
-	};
+	
+	ASSERT_TRUE(typeid(testVector) == typeid(vector<string>)) << "The expected return type vector<string> - The actual return type : " << typeid(testVector).name();
+	ASSERT_TRUE(testVector.size() == 3) << "The size of the test vector : 3 - The size of the returned Vactor : " << testVector.size();
 	
 }
 
@@ -65,7 +52,7 @@ TEST(TypeParcingTests, TypeParcing_Vector_bool) {
 
 	JSON testJson = ReadAndParseTypeTestFile(filePath);
 
-	cout << "TypeParcing_Vecotor  - > File Read OKAY" << endl;
+	
 
 	JSON result = GetValueByKey(testJson, "test_vector");
 
@@ -79,30 +66,23 @@ TEST(TypeParcingTests, TypeParcing_Vector_bool) {
 	SCOPED_TRACE(format("The size of the test vector : 3 - The size of the returned Vactor : {}", testVector.size()));
 	ASSERT_TRUE(testVector.size() == 3);
 
-	for (bool val : testVector) {
-		cout << format("Vector Element : {}", val) << endl;
-
-	};
+	
 }
 
-// Test fails
-// Returns vecor of size 0 
 
-// The heldtype is shred_ptr -> needs to be unpacked correctly
 TEST(TypeParcingTests, TypeParcing_nested_vector_double) {
 
 	string filePath = string(TYPE_TEST_FILE_PATH) + "vector/vector_type_nested_double.txt";
 	JSON testJson = ReadAndParseTypeTestFile(filePath);
 
-	cout << "TypeParcing_nested_vector_double -> File read OKAY" << endl;
+	
 
 	JSON result = GetValueByKey(testJson, "test_vector");
 
 	// should be vector<vector<double>>
 	vector<vector<double>> testVector = GetValueByKeyWithType<vector<vector<double>>>(testJson, "test_vector");
 
-	cout << "TypeParcingTests -> Returned vector size : " << testVector.size() << endl;
-
+	
 
 	SCOPED_TRACE(format("Expected the size of the testVector to be non 0 : size found {}", testVector.size()));
 	ASSERT_TRUE(testVector.size() > 0);
